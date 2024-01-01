8a9e615b382bc57ab24bfde14c6aa3daf01782fb
= <ansariovesh@yahoo.in>
2024-01-01 16:31:26 +0530

    error handling middlewares added

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import express from "express";
+
 import authRoutes from "./routes/auth-routes";
 import pool from "./db/db";
+import { errorHandler } from "./middlewares/error-handler";
+import { asyncHandler } from "./middlewares/async-handler";
+import { notFoundHandler } from "./middlewares/not-found-handler";
 
 const app = express();
 const port = 3000;
@@ -10,6 +14,20 @@ app.use(express.json());
 
 // Authentication routes
 app.use("/auth", authRoutes);
+
+// Error handling middleware
+app.use(errorHandler);
+
+// Async handler middleware example
+app.post(
+  "/async-example",
+  asyncHandler(async (req, res) => {
+    // Asynchronous logic here
+  })
+);
+
+// 404 handler middleware
+app.use(notFoundHandler);
 
 pool.connect((err: any, client: any) => {
   if (err) {
